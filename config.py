5ea45293e23e1dc33f3bda794d17f7ba0c9e165e
NoHOPE <240096962+NoHOPEC@users.noreply.github.com>
2026-03-05 08:04:47 +0530

    umm !!

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config:
     OWNER_ID = int(os.getenv("OWNER_ID"))
     MONGO_URI = os.getenv("MONGO_URI")
     
-    API_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    API_ID = 38145963
+    API_HASH = "9325201ac0b1f87528cede06dd88484d"
